661937da80fc4a12d2ba579f720631090441c7f6
binyu850906 <73628714+binyu850906@users.noreply.github.com>
2021-07-13 14:28:51 +0800

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by binyu on 2021/7/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
